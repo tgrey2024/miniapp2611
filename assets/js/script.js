27b36e64480f2185d917aa965f5f28d9289c8f26
Tripta Grey <tripta.grey@gmail.com>
2024-11-28 13:25:06 +0000

    change to option buttons with correct and incorrect answers highlighted

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,11 @@ document.addEventListener('DOMContentLoaded', function () {
     let correctNum = 0;
     // set incorrectNum as number of questions answered incorrectly
     let incorrectNum = 0;
+    // set score as percentage of correct answers
+    let score = 0;
+    // set feedbackElement to display feedback to user
+    let feedbackElement = document.getElementById('feedback');
+    
         
     const quizQuestions = [
     {
@@ -39,18 +44,47 @@ document.addEventListener('DOMContentLoaded', function () {
 // event listener for nextQuestionButton
 document.getElementById('nextButton').addEventListener("click",nextQuestion);
 
+// event listener for submitAnswerButton
+document.getElementById('submitButton').addEventListener("click",submitAnswer);
+
 // displays the next question
 function nextQuestion(){
     qnum++;
     buildQuiz();
-
+    feedbackElement.innerText = "";
 }
 
 // submit answer
-function submitAnswer(){
+function submitAnswer(e){
     // check if userAnswer matches correctAnswer in quizQuestions array
-    // correctNum++
-    // else incorrectNum++
+    const answerElement = e.currentTarget;
+    let answer = answerElement.getAttribute('data-value');
+    //let userAnswer = document.querySelector('input[type="radio"]:checked');
+    if (answer) {
+        if (answer === quizQuestions[qnum].correctAnswer) {
+            // change button color to green with white text, move to CSS later
+            answerElement.style.backgroundColor = "#206537";
+            answerElement.style.color = "#ffffff";
+            correctNum++;
+            score = Math.round((correctNum / (correctNum + incorrectNum)) * 100);
+            // display correct feedback
+            feedbackElement.innerText = "Correct! Well done!";
+        } else {
+            // change button color to green with white text, move to CSS later
+            answerElement.style.backgroundColor = "#710005";
+            answerElement.style.color = "#ffffff";
+            incorrectNum++;
+            // display incorrect feedback
+            feedbackElement.innerText = quizQuestions[qnum].incorrectFeedback;
+
+        }
+    } else {
+        // handle case where no answer is selected
+        feedbackElement.innerText = "Please select an answer";
+    }
+    // display score
+    console.log(score);
+    
 }
 
 // build the quiz page for a specific question in the quizQuestions array
@@ -70,12 +104,20 @@ function buildQuiz(){
     let options = quizQuestions[qnum].options;
     let optionsArray = Object.keys(options);
     let optionSetDiv = document.getElementById('optionSet');
-    let optionSet ="";
+    let optionSet = "";
     for (let option of optionsArray) {
-        optionSet += `<div><input type='radio'>${option}) ${options[option]}</input></div>`;
+        optionSet += `<button type='button' class='optionButton' data-value='${option}'>${option}) ${options[option]}</button>`;
     }
     optionSetDiv.innerHTML = optionSet;
-}
+    // add event listener to option buttons
+    let optionButtons = document.querySelectorAll('.optionButton');
+    for (let button of optionButtons) {
+        button.addEventListener('click', submitAnswer); 
+            let selectedButton = button.getAttribute('data-value');
+            answer = selectedButton;
+        }
+    }
+    
 
     buildQuiz();
 });
